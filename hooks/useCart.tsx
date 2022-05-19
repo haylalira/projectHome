cad24ca785ddb942fec7663f41c2e5d59087743b
Deyvison Penha <deyvisonpenha1@gmail.com>
2022-05-19 20:37:20 +0000

    corrigindo erro

--- hooks/useCart.tsx
+++ hooks/useCart.tsx
@@ -35,11 +35,14 @@ const CartContext = createContext<CartContextData>({} as CartContextData);
 
 export function CartProvider({ children }: CartProviderProps): JSX.Element {
   const [cart, setCart] = useState<Product[]>(() => {
-     const storagedCart = localStorage.getItem('@RocketShoes:cart');
+    if (typeof window !== 'undefined') {
+      // Perform localStorage action
+      const storagedCart = localStorage.getItem('@RocketShoes:cart') || '';
       if (storagedCart) {
        return JSON.parse(storagedCart);
      }
-
+    }
+     
     return [];
   });
 
